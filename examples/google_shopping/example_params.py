37496ae09e863333f66514d5b9ac6ff17dfa2e7f
Chukhrai Artur <chukhraiartur@gmail.com>
2023-05-30 01:02:12 +0300

    feat(google_shopping): added Google Shopping parser

---
+++ examples/google_shopping/example_params.py
@@ -0,0 +1,10 @@
+from google_shopping import GoogleShoppingParams
+
+domains = GoogleShoppingParams.get_domains()
+print(domains)
+
+countries = GoogleShoppingParams.get_countries()
+print(countries)
+
+languages = GoogleShoppingParams.get_languages()
+print(languages)
---
+++ google_shopping/__init__.py
@@ -0,0 +1,1 @@
+from .google_shopping import GoogleShoppingParams, GoogleShoppingSearch, GoogleShoppingProduct
